cd4dc4bd9a8f7b15056a96258b9ea8cc37dbef8c
Petr Portnov <me@progrm-jarvis.ru>
2021-08-23 16:11:12 +0300

    feat: fully implement `Dynamic` including `update(..)`

--- zaraza-common-api/src/main/java/ru/divinecraft/zaraza/common/api/reactor/Dynamic.java
+++ zaraza-common-api/src/main/java/ru/divinecraft/zaraza/common/api/reactor/Dynamic.java
@@ -18,7 +18,9 @@ import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.val;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NonBlocking;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.reactivestreams.Publisher;
@@ -26,86 +28,155 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
+import java.util.function.UnaryOperator;
+
 /**
- * Represent any value slot<br>
- * Last value is cached and can be gotten by {@link #latest()} method
+ * <p>A dynamically changed value.</p>
+ * <p>The last observed value is always cached
+ * and can be synchronously retrieved via call to {@link #snapshot()}.</p>
  *
- * @param <T> value type
+ * @param <T> type of dynamic value
  */
+@NonBlocking
 public interface Dynamic<T> {
 
     /**
-     * Returns latest pushed element<br>
-     * The invocation of {@code latest()} is equivalent to:
-     * <pre>asFlux().blockFirst()</pre>
-     * <li>Note that returned value may NOT be ACTUAL value</li>
+     * Returns the latest observed (cached) value.
      *
-     * @return cached value
+     * @return the cached value
+     *
+     * @apiNote the returned value is the latest observed snapshot and may be globally "old",
+     * as there may be a foreground process eventually leading to the value being changed
      */
-    @Nullable T latest();
+    @Nullable T snapshot();
 
     /**
-     * Returns flux than cache last pushed element, so {@link Flux#blockFirst()}
-     * returns latest element without any blocks
+     * <p>Asynchronously attempts to set this value to the new state.</p>
+     * <p>The operation succeeds iff compare-and-set from currently observed value to the new one succeeds.</p>
+     *
+     * @param newValue the new value
+     * @return mono emitting {@code true} on success or {@code false} if the value was not changed to the provided one
+     *
+     * @apiNote the value is not directly set via call to this method,
+     * it will only get changed via an external call to update
+     */
+    @NotNull Mono<Boolean> trySet(@NotNull T newValue);
+
+    /**
+     * Updates this dynamic value using the provided operator.
+     *
+     * @param operator function used to convert the current value to the new one
+     * @return mono emitting the resulting value
+     *
+     * @apiNote operator may be invoked multiple times
+     */
+    @NotNull Mono<? extends T> update(@NotNull UnaryOperator<T> operator);
+
+    /**
+     * <p>Returns a {@link Flux}-view of this dynamic value emitting values on its updates.</p>
+     * <p>The current value will always be emitted as the first signal.</p>
      *
      * @return Flux view of this dynamic
      */
-    @NotNull Flux<T> asFlux();
+    @NotNull Flux<? extends T> asFlux();
 
     /**
-     * Asynchronously sets new value for this Dynamic and sends updates if success<br>
-     * Mono signal indicates was operation successful or not (true for success)<br>
-     * If no any value was cached (method {@link #latest()} returns null), then operation would not completed<br>
+     * Creates a new dynamic value.
      *
-     * @param value new value
-     * @return operation mono
+     * @param externalWriter function used to perform atomic compare-and-set of the value globally
+     * @param publisher publisher emitting values on external value changes
+     * @param initialValue initial value
+     * @param <T> type of dynamic value
+     * @return new dynamic value
+     *
+     * @throws NullPointerException if {@code externalWriter} is {@code null}
+     * @throws NullPointerException if {@code publisher} is {@code null}
+     * @throws NullPointerException if {@code initialValue} is {@code null}
      */
-    @NotNull Mono<Boolean> set(@NotNull T value);
+    @Contract("null, _, _ -> fail;"
+            + "_, null, _ -> fail;"
+            + "_, _, null -> fail;"
+            + "_, _, _ -> new;")
+    static <T> @NotNull Dynamic<T> create(final @NonNull CompareAndSetOperation<@NotNull ? super T> externalWriter,
+                                          final @NonNull Publisher<? extends T> publisher,
+                                          final @NonNull T initialValue) {
+        val sink = Sinks.many().replay().latestOrDefault(initialValue);
+        // TODO: handle emit errors
+        Flux.from(publisher)
+                .subscribe(
+                        value -> sink.emitNext(value, Sinks.EmitFailureHandler.FAIL_FAST),
+                        error -> sink.emitError(error, Sinks.EmitFailureHandler.FAIL_FAST),
+                        () -> sink.emitComplete(Sinks.EmitFailureHandler.FAIL_FAST)
+                );
 
-    /**
-     * Creates new Dynamic instance
-     *
-     * @param writer       function that saves new value and generate signals on successful write
-     * @param publisher    value updates publisher. Supplies update signals when value was changed
-     * @param initialValue first value that will be cached until publisher generates any value update
-     * @param <T>          value type
-     * @return new Dynamic
-     */
-    @Contract("_,_,_->new")
-    static <T> Dynamic<T> create(@NonNull CompareAndSetOperation<T> writer, @NonNull Publisher<T> publisher, @NotNull T initialValue) {
-        Sinks.Many<T> sink = Sinks.many().replay().latestOrDefault(initialValue);
-        Flux.from(publisher).subscribe(sink::tryEmitNext, sink::tryEmitError, sink::tryEmitComplete);
-        return new ReactorDynamic<>(sink,
-                sink.asFlux(),//replay sink cache last value manually. BTW #asFlux() for ReplayingSink returns its Sink instance
-                writer);
+        return new ReactorDynamic<>(sink.asFlux(), externalWriter);
     }
 
+    /**
+     * {@link Dynamic} implementation which stores the {@link Flux} representation of {@link Sinks.Many}.
+     *
+     * @param <T> type of dynamic value
+     */
+    //@formatter:off (don't merge last annotation and class declaration lines)
     @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
     @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
     final class ReactorDynamic<T> implements Dynamic<T> {
-        Sinks.Many<T> sink;
-        Flux<T> asFlux;
-        CompareAndSetOperation<T> writer;
+        //@formatter:on
+
+        /**
+         * {@link Flux} representation of replaying multicast {@link Sinks.Many}.
+         */
+        @NotNull Flux<? extends T> asFlux;
+
+        /**
+         * Operation responsible for performing atomic compare-and-swap.
+         */
+        @NotNull CompareAndSetOperation<@NotNull ? super T> externalWriter;
 
         @Override
-        public @Nullable T latest() {
-            return asFlux().blockFirst();
+        public @Nullable T snapshot() {
+            // non-blocking: `asFlux` is *replaying* thus it always produces the value instantly
+            return asFlux.blockFirst();
         }
 
         @Override
-        public @NotNull Flux<T> asFlux() {
+        public @NotNull Flux<? extends T> asFlux() {
             return asFlux;
         }
 
         @Override
-        public @NotNull Mono<Boolean> set(@NotNull T value) {
-            Mono<Boolean> mono = Mono.fromCallable(() -> {
-                T old;
-                if ((old = latest()) == null) return false;
-                return writer.compareAndSet(old, value);
-            });
-            mono.subscribe();
-            return mono;
+        public @NotNull Mono<Boolean> trySet(final @NotNull T newValue) {
+            return asFlux.next()
+                    .map(current -> externalWriter.compareAndSet(current, newValue));
+        }
+
+        @Override
+        public @NotNull Mono<? extends T> update(final @NotNull UnaryOperator<T> updater) {
+            return Mono.defer(() -> asFlux.next().flatMap(oldValue -> {
+                final T newValue;
+                return trySet(newValue = updater.apply(oldValue))
+                        .flatMap(successful -> successful ? Mono.just(newValue) : Mono.error(ShouldRetry.INSTANCE));
+            })).retry();
+        }
+
+        /**
+         * Pseudo-error used to implement retry logic of {@link #update(UnaryOperator)}.
+         */
+        private static final class ShouldRetry extends Throwable {
+
+            /**
+             * Singleton instance of this pseudo-error
+             */
+            private static final Throwable INSTANCE = new ShouldRetry(
+                    null, null, true, false
+            );
+
+            public ShouldRetry(final @Nullable String message,
+                               final @Nullable Throwable cause,
+                               final boolean enableSuppression,
+                               final boolean writableStackTrace) {
+                super(message, cause, enableSuppression, writableStackTrace);
+            }
         }
     }
 }
--- zaraza-common-api/src/test/java/ru/divinecraft/zaraza/common/api/reactor/DynamicTest.java
+++ zaraza-common-api/src/test/java/ru/divinecraft/zaraza/common/api/reactor/DynamicTest.java
@@ -14,26 +14,83 @@
 
 package ru.divinecraft.zaraza.common.api.reactor;
 
+import lombok.val;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DynamicTest {
 
     @Test
-    void cashingInitialValueTest() {
-        Dynamic<String> dyn = Dynamic.create((i, n) -> true, Flux.empty(), "foo");
-        assertEquals(dyn.latest(), "foo");
-        assertEquals(dyn.latest(), "foo");
-        assertEquals(dyn.latest(), "foo");
+    void dynamic_create__multicasts() {
+        val sink = Sinks.many()
+                .unicast()
+                .<String>onBackpressureError();
+        val dynamic = Dynamic.create((oldValue, newValue) -> {
+            sink.emitNext(newValue, Sinks.EmitFailureHandler.FAIL_FAST);
+            return true;
+        }, sink.asFlux(), "initial");
+
+        assertDynamicReplays(dynamic, "initial", 1, 3, 54, 4, 64, 3, 2);
+
+        StepVerifier.create(dynamic.trySet("new"))
+                .expectNext(true)
+                .expectComplete()
+                .verify();
+
+        assertDynamicReplays(dynamic, "new", 12, 12, 1, 3, 2, 2, 244, 357, 8);
     }
 
     @Test
-    void cashingSomeReceivedValueTest() {
-        Dynamic<String> dyn = Dynamic.create((i, n) -> true, Flux.just("bar", "nobar", "hello"), "foo");
-        assertEquals(dyn.latest(), "hello");
-        assertEquals(dyn.latest(), "hello");
+    void dynamic_create__update() {
+        val attemptsBeforeSuccess = new AtomicInteger(77);
+
+        val sink = Sinks.many()
+                .unicast()
+                .<String>onBackpressureError();
+        val dynamic = Dynamic.create((oldValue, newValue) -> {
+            // succeed CAS only after multiple attempts
+            if (attemptsBeforeSuccess.decrementAndGet() == 0) {
+                sink.emitNext(newValue, Sinks.EmitFailureHandler.FAIL_FAST);
+
+                return true;
+            }
+
+            return false;
+        }, sink.asFlux(), "foo");
+
+        assertDynamicReplays(dynamic, "foo", 121, 21, 486, 4, 525, 653, 364, 4);
+
+        StepVerifier.<String>create(dynamic.update(old -> {
+                    assertEquals("foo", old, "CAS request produces the invalid result");
+                    return "bar";
+                }))
+                .expectNext("bar")
+                .expectComplete()
+                .verify();
+        assertDynamicReplays(dynamic, "bar", 12, 46, 4735, 7, 3, 4, 45, 45, 35, 3);
     }
 
+    private static <T> void assertDynamicReplays(final @NotNull Dynamic<T> dynamic,
+                                                 final @NotNull T expectedValue,
+                                                 final int @NotNull ... attempts) {
+        val attemptsLength = attempts.length;
+        for (var attemptsGroup = 0; attemptsGroup < attemptsLength; attemptsGroup++) {
+            val currentAttempts = attempts[attemptsGroup];
+            val asFlux = attemptsGroup % 2 == 0;
+            //@formatter:off
+            for (int attempt = 0; attempt < currentAttempts; attempt++) if (asFlux) StepVerifier
+                    .<T>create(dynamic.asFlux())
+                    .expectNext(expectedValue)
+                    .thenCancel()
+                    .verify();
+            else assertEquals(expectedValue, dynamic.snapshot());
+            //@formatter:on
+        }
+    }
 }
